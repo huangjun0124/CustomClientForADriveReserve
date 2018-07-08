1f11682795b97574104d30e7e2f0a25e701546c9
keith <keithhuang0124@gmail.com>
2018-07-08 19:08:14 +0800

    update comment

--- PPYC/ServerInteract/GetUncommentedReserve.cs
+++ PPYC/ServerInteract/GetUncommentedReserve.cs
@@ -13,7 +13,7 @@ namespace PPYC.ServerInteract
         {
             var request = GetNewRequest(code, UrlManager.GetMyApp_CarOrderInfoNew);
             request.AddParameter("AppRecordID", appRecordId);
-            request.AddParameter("State", 2);
+            request.AddParameter("State", 2); // 订单状态 (1:已预约;2:待评价;0:已取消;4:已评论)
             return RequestClient.DoWithRequest<ApiGetUncommentedReserveResult>(request);
         }
     }
